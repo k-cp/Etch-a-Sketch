9a6841098f5a9a2159f6df158518fab27f5afbd2
Kaya Arai-Yokoi <kaya.araiyokoi@gmail.com>
2023-08-14 17:15:22 +0200

    Create grid

--- js-file.js
+++ js-file.js
@@ -1,8 +1,23 @@
+const div = document.querySelector('.container')
+div.style.height = '640px';
+div.style.width = '640px';
+
 for (let i = 0; i < 16; i++) {
     //create row div
-
+    const row = document.createElement('div');
+    row.classList.add('row');
     //create 16div inside row div
     for (let j = 0; j < 16; j++) {
-        d
+        const box = document.createElement('div');
+        box.style.backgroundColor = "orange";
+        box.style.border = 'solid black';
+        box.style.minBlockSize = `${parseInt(div.style.height)/16}px`;
+        box.style.boxSizing = 'border-box';
+        box.style.flex = '1';
+        box.classList.add('box');
+        row.appendChild(box);
     }
+    div.appendChild(row);
 }
+
+const box = document.querySelectorAll('.box');
