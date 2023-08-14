494bd6c9725e3feb324d6ad21ad6cf21568b6d76
Kaya Arai-Yokoi <kaya.araiyokoi@gmail.com>
2023-08-14 20:21:27 +0200

    Add slider to edit grid size

--- js-file.js
+++ js-file.js
@@ -18,13 +18,39 @@ for (let i = 0; i < 16; i++) {
     }
     div.appendChild(row);
 }
-
-const boxes = document.querySelectorAll('.box');
-boxes.forEach(box => box.addEventListener('mouseover', changeColor))
-
-const clear = document.querySelector('#clear');
-
+let boxes = document.querySelectorAll('.box');
+boxes.forEach(box => box.addEventListener('mouseover', changeColor));
+let clear = document.querySelector('#clear');
 clear.addEventListener('click', clearColor);
+
+const output = document.querySelector('#output');
+const slider = document.querySelector('.slider');
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+    clearBox(".container");
+    for (let i = 0; i < this.value; i++) {
+        //create row div
+        const row = document.createElement('div');
+        row.classList.add('row');
+        //create 16div inside row div
+        for (let j = 0; j < this.value; j++) {
+            const box = document.createElement('div');
+            box.style.border = 'solid black';
+            box.style.minBlockSize = `${parseInt(div.style.height)/this.value}px`;
+            box.style.boxSizing = 'border-box';
+            box.style.flex = '1';
+            box.classList.add('box');
+            row.appendChild(box);
+        }
+        div.appendChild(row);
+    }
+    boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => box.addEventListener('mouseover', changeColor))
+    clear.addEventListener('click', clearColor);
+
+  }
 
 function changeColor(e) {
     const selected = e.target;
@@ -36,4 +62,9 @@ function clearColor() {
         const selected = box;
         selected.style.backgroundColor = 'white';
     })
+}
+
+function clearBox(elementClass)
+{
+    document.querySelector(`${elementClass}`).innerHTML = "";
 }
